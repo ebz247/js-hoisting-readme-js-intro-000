a98e2d777297387d9ba56a4028d81e5e92dfbd9e
eb247 <eb247@hotmail.com>
2018-10-04 12:00:56 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,17 @@
-function callMe() {
+function callMe(){
   var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-
 }
 
-
-function crazy() {
-  // fix the code in here:
-
+function crazy(){
+  //fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-
   thisIsCrazy();
 }
 
@@ -27,6 +23,7 @@ function sayMyName() {
 
   function sayMy() {
     var name = "Kristin";
+
     console.log(name);
   }
 }
